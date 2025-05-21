cf5298f912fd8c1f26107a044378aeae9b31d11e
junereo <taejung.joe.kim@gmail.com>
2025-05-21 10:09:58 +0900

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: ['react', 'react-hooks'],
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended',
+      'prettier',
+    ],
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  };
+  
